6dee51c8decd955bf75ed18e92c5a8308ae3c6e0
vjspranav <pranavasri@live.in>
2019-07-13 21:23:06 +0200

    Revert "Shift to Neutrino Kernel"
    This reverts commit bee1ce37d4c651a1988a43d58c15cdb17999be93.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -3,7 +3,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may btain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -40,7 +40,7 @@ TARGET_BOOTLOADER_BOARD_NAME := sdm845
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := neutrino_fajita_defconfig
+TARGET_KERNEL_CONFIG := kronic_defconfig
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
@@ -53,8 +53,6 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 # Clang
 TARGET_KERNEL_CLANG_COMPILE := true
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-CROSS_COMPILE_ARM32 = prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 TARGET_KERNEL_CLANG_VERSION := 9.0.5
 
 # Platform
